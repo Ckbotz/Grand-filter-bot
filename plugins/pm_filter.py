c63355f2a08ad352835c8fa64baf2a9ab9bdc03e
kichu <kichuxpro@outlook.com>
2026-07-11 00:23:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,7 +97,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                   text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                   text=f"{get_size(file.file_size)}|{tag} {name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -106,10 +106,10 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"{get_size(file.file_size)}|{tag} {name}", callback_data=f'{pre}#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}',
+                    text=f"{get_size(file.file_size)}|{tag} {name}", callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
             for file in files
@@ -1281,7 +1281,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"{get_size(file.file_size)}|{tag} {name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1290,10 +1290,10 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"{get_size(file.file_size)}|{tag} {name}", callback_data=f'{pre}#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"{get_size(file.file_size)}|{tag} {name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
